107d0883c65864c863d57e6f0a5d17e65a27c76a
emmaprati <167798217+emmaprati@users.noreply.github.com>
2025-01-03 17:57:27 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="../css/styleLogin.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="intestazione">
+
+        <a href="home.html"><img src="../img/Project Work(1).png" alt="logo" height="70px" class="int-logo"></a>
+        <a href="home.html">
+            <h1 class="int-title">TouchRoom</h1>
+        </a>
+
+        <div id="int-bottoni">
+            <a href="signUp.html"><button class="signup" role="button">Sign Up</button></a>
+            <a href="home.html"><button class="home" role="button">Home</button></a>
+        </div>
+
+    </div>
+
+    <div class="main-content">
+        <div class="login-container">
+            <h1>Log in</h1>
+            <form>
+                <input type="email" placeholder="email" class="input-field" name="email" required>
+                <input type="password" placeholder="password" class="input-field" name="password" required>
+                <div class="checkbox-container">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Ricordami</label>
+                </div>
+                <button type="submit" class="login-button">accedi</button>
+            </form>
+        </div>
+</body>
+<script src="../script/scriptLogin.js"></script>
+
+</html>
