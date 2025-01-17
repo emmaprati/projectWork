5c23bd52d11f20d6f12ffa005fd79bc381d268b0
emmaprati <emma.prati06@gmail.com>
2025-01-17 19:11:59 +0100

    menu

--- script/scriptPrenotazione.js
+++ script/scriptPrenotazione.js
@@ -2,12 +2,32 @@ const toggleButton = document.getElementById("toggle-btn");
 const sidebar = document.getElementById("sidebar");
 
 function toggleSidebar() {
-    sidebar.classList.toggle("close");
-    toggleButton.classList.toggle("rotate");
+    sidebar.classList.toggle("close")
+    toggleButton.classList.toggle("rotate")
+    closeAllSubMenu();
 
 }
 
 function toggleSubMenu(button) {
+
+    if(!button.nextElementSibling.classList.contains("show")){
+        closeAllSubMenu();
+    }
+
     button.nextElementSibling.classList.toggle("show");
     button.classList.toggle("rotate");
+
+    if (sidebar.classList.contains("close")) {
+        sidebar.classList.toggle("close");
+        toggleButton.classList.toggle("rotate");
+    }
+
+}
+
+function closeAllSubMenu() {
+    Array.from(sidebar.getElementsByClassName("show")).forEach(ul => {
+        ul.classList.remove("show");
+        ul.previousElementSibling.classList.remove("rotate");
+
+    });
 }
